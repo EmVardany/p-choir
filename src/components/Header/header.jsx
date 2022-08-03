d6c2b949c5c0669c0b98b3e759d273f449a51eeb
Emmanuil Vardanian <em.vardany@gmail.com>
2022-08-03 14:52:36 +0300

    change font in header, logo and background

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,14 +1,16 @@
 import classes from './header.module.css';
-import logo from '../../assets/backgrounds/header-logo.svg';
-import root from "react-dom";
+import logo from '../../assets/backgrounds/header-logo.png';
 
 const Header = () => {
-    root.render(
+    return(
         <header className={classes.header}>
             <img src={logo} alt="logo" className={classes.logo}/>
-            <p className={classes.verse}><span>Хвали, душе моя, Господа</span> <span>Пс. 146:1</span></p>
+            <p className={classes.verse}>
+                <span>Хвали, душе моя, Господа</span>
+                <span>Пс. 146:1</span>
+            </p>
         </header>
-    , document.getElementById('root'))
+    )
 }
 
 export default Header;
