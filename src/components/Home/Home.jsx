fa25a5cd3f19587778238267340c7b6fd825de81
Emmanuil Vardanian <em.vardany@gmail.com>
2022-09-13 10:49:16 +0300

    redesign home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,17 @@
 import {NavLink} from "react-router-dom";
 import classes from "./Home.module.css"
+import musicIcon from "../../assets/backgrounds/music.svg"
 
 const Home = (props) => {
     return (
-        <section className={classes.home}>
+        <section>
             <ul className={classes.songList}>
                 {
                     props.songsList.map((song, key) => (
-                            <li key={key}
-                                className={classes.item}>
-                                <NavLink to={`/${song.url_title}`}
-                                         onClick={() => props.setCurSong(song)}>
-                                    {song.title}
+                            <li key={key} className={classes.item}>
+                                <NavLink to={`/${song.url_title}`}  onClick={() => props.setCurSong(song)}>
+                                    <img src={musicIcon} alt="music" className={classes.musicIcon}/>
+                                    <span>{song.title}</span>
                                 </NavLink>
                             </li>
                         )
